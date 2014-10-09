4e457372d51943dbe37d60aeb0762526e74230f9
whoistim <whoistim@sbcglobal.net>
2014-10-08 17:51:08 -0700

    hi tim

--- spec/test.js
+++ spec/test.js
@@ -7,36 +7,39 @@ expect = require('chai').expect;
 describe('Car', function(){
 
   beforeEach(function(){
+    myCar = new Car ("Fiat", "Panda", new Date().getFullYear(), "White");
     // create a new myCar object each time
   });
 
   describe('#year', function(){
     //think about using the new Date() and getFullYear functions
     it('should be the current year', function(){
-
+      expect(myCar.year).to.equal(new Date().getFullYear());
     });
   });
 
   describe('#state', function(){
     it('should initially be off', function(){
-
+      expect(myCar.state).to.equal("off");
     });
   });
 
   describe('#previousOwners', function(){
     it('should initially be empty', function(){
-
+      expect(myCar.previousOwners.length).to.equal(0);
     });
   });
 
-  describe('#curretOwner', function(){
+  describe('#currentOwner', function(){
     it('should initially be manufacturer', function(){
+      expect(myCar.currentOwner).to.deep.equal(myCar.make);
 
     });
   });
 
   describe('#passengers', function(){
     it('should initially be empty', function(){
+      expect(myCar.passengers.length).to.equal(0);
 
     });
   });
@@ -44,29 +47,35 @@ describe('Car', function(){
   describe('#sale', function(){
 
     it('should move currentOwner to previousOwners array', function(){
-
+      myCar.sale("Bob");
+      expect(myCar.previousOwners).to.contain("Fiat");
     });
 
     it('should update currentOwner with the new owner', function(){
-
+      myCar.sale("Bob");
+      expect(myCar.currentOwner).to.equal("Bob");  
     });
   });
 
   describe('#paint', function(){
-    it('should update the color of myCar', function(){
+    it('should update the color of myCar to blue', function(){
+      myCar.paint("blue");
+      expect(myCar.color).to.equal("blue");  
 
     });
   });
 
   describe('#start', function(){
     it('should update the state to on', function(){
-
+      myCar.start();
+      expect(myCar.state).to.equal("on");
     });
   });
 
   describe('#off', function(){
     it('should update the state to off', function(){
-
+      myCar.off();
+      expect(myCar.state).to.equal("off");
     });
   });
 
--- src/car.js
+++ src/car.js
@@ -1,13 +1,29 @@
 function Car(make, model, year, color){
-
+	this.make = make;
+	this.model = model;
+	this.year = year;
+	this.color = color;
+	this.state = "off";
+	this.previousOwners = [];
+	this.currentOwner = make;
+	this.passengers = [];
 }
 
 Car.prototype.sale = function(newOwner){
-
+	this.previousOwners.push(this.currentOwner);
+	this.currentOwner = newOwner;
 };
 
 Car.prototype.paint = function(newColor){
+	this.color = newColor;
+};
 
+Car.prototype.start = function() {
+	this.state = "on";
+};
+
+Car.prototype.off = function() {
+	this.state = "off";
 };
 
 module.exports=Car;
